7f44670ea0fdbb451e4a36d513fec5b8f921b66a
Kristijan <kjagunec@gmail.com>
2024-02-26 14:30:47 +0100

    Added products page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from "./main/main.component";
 import {AboutComponent} from "./about/about.component";
+import {ProductsComponent} from "./products/products.component";
 
 export const routes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'products', component: ProductsComponent}
 ];
