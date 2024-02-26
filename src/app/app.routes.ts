8b02147262b745b2e2381b8e738db17520958be1
Kristijan <kjagunec@gmail.com>
2024-02-26 14:55:03 +0100

    Added news page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {MainComponent} from "./main/main.component";
 import {AboutComponent} from "./about/about.component";
 import {ProductsComponent} from "./products/products.component";
+import {NewsComponent} from "./news/news.component";
 
 export const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'products', component: ProductsComponent}
+  {path: 'products', component: ProductsComponent},
+  {path: 'news', component: NewsComponent}
 ];
