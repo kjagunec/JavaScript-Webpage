621ac55fd6e7b94dd4e9ddfbf9039ee8e932b8af
Kristijan <kjagunec@gmail.com>
2024-03-05 17:40:15 +0100

    Added user and post models

---
+++ src/app/shared/models/post.model.ts
@@ -0,0 +1,5 @@
+export class Post {
+  id : number = 0;
+  header : string = "";
+  text : string = "";
+}
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Post} from "../models/post.model";
+import {User} from "../models/user.model";
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +18,27 @@ export class DataService {
 
   constructor(private http : HttpClient) { }
 
-  getPosts() {
+  getUsers() {
+    return this.http.get<Observable<{status: string, users : User[]}>>(this.apiRootUsers);
+  }
 
+  addUser(user : User) {
+    return this.http.post<Observable<{status : string, insertId : number}>>(this.apiRootUsers, user);
+  }
+
+  getPosts() {
+    return this.http.get<Observable<{status : string, posts : Post[]}>>(this.apiRootPosts);
+  }
+
+  addPost(post:Post) {
+    return this.http.post<Observable<{status:string, insertId:number}>>(this.apiRootPosts, post);
+  }
+
+  deletePost(id:number) {
+    return this.http.delete(this.apiRootPosts + `/${id}`);
+  }
+
+  editPost(post:Post) {
+    return this.http.put(this.apiRootPosts, post);
   }
 }
